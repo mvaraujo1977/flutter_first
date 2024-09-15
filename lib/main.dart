afca8ae96f677f4e7133ea96f7dac99c88c7420e
Marcelo Viana <marcelovianadearaujo@gmail.com>
2024-09-15 19:08:35 -0300

    colocando Butão

--- lib/main.dart
+++ lib/main.dart
@@ -11,42 +11,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(height: 100, width: 100, color: Colors.red),
-              Container(height: 50, width: 50, color: Colors.blue),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(height: 100, width: 100, color: Colors.blue),
-              Container(height: 50, width: 50, color: Colors.red),
-            ],
-          ),
-          Row(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Container(
+          color: Colors.white,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(height: 50, width: 50, color: Colors.blue),
-              Container(height: 50, width: 50, color: Colors.red),
-              Container(height: 50, width: 50, color: Colors.green),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(height: 100, width: 100, color: Colors.red),
+                  Container(height: 50, width: 50, color: Colors.blue),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(height: 100, width: 100, color: Colors.blue),
+                  Container(height: 50, width: 50, color: Colors.red),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(height: 50, width: 50, color: Colors.blue),
+                  Container(height: 50, width: 50, color: Colors.red),
+                  Container(height: 50, width: 50, color: Colors.green),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text(
+                  'Diamante Amarelo',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Você apertou o Botão');
+                },
+                child: Text('Aperte o botão!'),
+              ),
             ],
-          )
-
-        ]
-
-      )
-    );
+          ),
+        ));
   }
 }
